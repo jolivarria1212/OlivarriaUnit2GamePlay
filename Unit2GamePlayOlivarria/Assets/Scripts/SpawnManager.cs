83f20d786af15f38a1f91463f57761afe78c174c
Jose Olivarria <145067078+jolivarria1212@users.noreply.github.com>
2024-03-20 13:48:46 -0700

    DONE

--- Unit2GamePlayOlivarria/Assets/Scripts/SpawnManager.cs
+++ Unit2GamePlayOlivarria/Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20; 
+    private float spawnRangeX = 20f;
+    private float spawnPosZ = 20f;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
         
     }
 
@@ -18,10 +22,14 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
         
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
