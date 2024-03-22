eea4e75fb7f2bcd2f90ac3a97623b947f3c67bd3
Jose Olivarria <145067078+jolivarria1212@users.noreply.github.com>
2024-03-22 13:44:22 -0700

    Finished

--- Unit2GamePlayOlivarria/Assets/Scripts/PlayerController.cs
+++ Unit2GamePlayOlivarria/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
     public float horizontalInput;
-    public float xRange = 10;
-    public GameObject projecttilePrefab;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
+    public Transform projectileSpawnPoint;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,17 +27,29 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-        if(transform.position.x > xRange)
+        if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(projecttilePrefab, transform.position, projecttilePrefab.transform.rotation); 
+            Instantiate(projectilePrefab, projectileSpawnPoint.position, projectilePrefab.transform.rotation);
         }
-       
-        
     }
 }
--- Unit2GamePlayOlivarria/Assets/Scripts/SpawnManager.cs
+++ Unit2GamePlayOlivarria/Assets/Scripts/SpawnManager.cs
@@ -5,31 +5,51 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20f;
-    private float spawnPosZ = 20f;
-    private float startDelay = 2;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     private float spawnInterval = 1.5f;
-
+    private float startDelay = 2;
+    public float sideSpawnMaxZ;
+    public float sideSpawnMinZ;
+    public float sideSpawnX;
+    private float spawnLeftInterval = 2.5f;
+    private float spawnRightInterval = 3.5f;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
-        
+        InvokeRepeating("SpawnRightAnimal", startDelay, spawnRightInterval);
+        InvokeRepeating("SpawnLeftAnimal", startDelay, spawnLeftInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-        }
-        
+
     }
+
     void SpawnRandomAnimal()
     {
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
         int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
         Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+
+    void SpawnLeftAnimal()
+    {
+
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(-sideSpawnX, 0, Random.Range(sideSpawnMinZ, sideSpawnMaxZ));
+        Vector3 rotation = new Vector3(0, 90, 0);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(rotation));
+    }
+
+    void SpawnRightAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(sideSpawnX, 0, Random.Range(sideSpawnMinZ, sideSpawnMaxZ));
+        Vector3 rotation = new Vector3(0, -90, 0);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(rotation));
     }
 }
